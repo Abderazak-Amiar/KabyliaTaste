a2ceb8666c3878ce1ba22aec76339aba4e92b110
Abderazak <Abderazak@DESKTOP-G5RPKGD>
2026-07-18 15:35:48 +0100

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,15 @@
+namespace KabyliaTaste
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
