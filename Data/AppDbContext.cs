3e0574a8bd0096bd7cdf1ef3ca307b3810def794
Abderazak <Abderazak@DESKTOP-G5RPKGD>
2026-07-18 16:00:57 +0100

    ci: install ef core packages

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using KabyliaTaste.Models;
+
+namespace KabyliaTaste.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=app.db");
+            }
+        }
+    }
+}
